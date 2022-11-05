771105180020f7f636e80671d2bc4c8598fded12
avinash1041 <jadhavavinash1041@gmail.com>
2022-11-05 10:29:09 +0530

    testing done

---
+++ quize_game.py
@@ -0,0 +1,49 @@
+print("Welcome to my computer quize!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! let's play :)")  
+
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('correct!')
+    score += 1
+else:
+    print('Incorrect!')
+    
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('correct!')
+    score += 1
+else:
+    print('Incorrect!')
+    
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print('correct!')
+    score += 1
+else:
+    print('Incorrect!')
+    
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print('correct!')
+    score += 1
+else:
+    print('Incorrect!')
+    
+    
+answer = input("What does ROM stand for? ")
+if answer.lower() == "read only memory":
+    print('correct!')
+    score += 1
+else:
+    print('Incorrect!')
+    
+print("you got " + str(score) + " questions correct!")
+print("you got " + str((score/5) * 100) + " %.")
